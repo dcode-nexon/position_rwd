932b87111156b94411628e8406e3bd1513c0b56d
tubi55 <tubi55@nate.com>
2022-11-28 16:03:21 +0900

    이벤트 연결 및 animation활용

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+const list = document.querySelector('.list');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
 
+prev.addEventListener('click', () => {
+	list.append(list.firstElementChild);
+
+	prev.classList.add('on');
+	setTimeout(() => prev.classList.remove('on'), 500);
+});
+
+next.addEventListener('click', () => {
+	list.prepend(list.lastElementChild);
+
+	next.classList.add('on');
+	setTimeout(() => next.classList.remove('on'), 500);
+});
